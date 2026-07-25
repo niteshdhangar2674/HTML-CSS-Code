77fcf7b8d5a94b52afd9d42cd702ab7484c26bd7
Nitesh Dhangar <niteshdhangar270@gmail.com>
2026-07-25 20:40:43 +0530

    Array methods

--- javaScript/t13.js
+++ javaScript/t13.js
@@ -6,8 +6,27 @@ let teams = [
                 memberName: "altaf"
             },
             {
-
+                memberName: "rahul"
             }
+        ]
+    },
+    {
+        leaderName: "Nitesh",
+        teamMember: [
+            { memberName: "mohan" },
+            { memberName: "sohan" }
         ]
     }
 ]
+
+
+let mems = teams.reduce((members, teams) => {
+    members.push(teams.leaderName);
+    teams.teamMember?.map(emem => {
+        members.push(emem.memberName);
+    })
+    return members;
+}, [])
+
+console.log(mems);
+
